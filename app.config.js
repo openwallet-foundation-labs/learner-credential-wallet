5716e071fa7c59d6cc94c0644aa2d2d51be0d04a
Alex H <ahiguera@Alexs-MacBook-Pro.local>
2025-02-28 22:26:46 -0500

    Release v2.1.8-build88
    Signed-off-by: Alex H <ahiguera@Alexs-MacBook-Pro.local>

--- app.config.js
+++ app.config.js
@@ -4,8 +4,8 @@
  */
 const env = process.env;
 
-const BUILD_NUMBER = parseInt(env.APP_BUILD_NUMBER) || 87;
-const VERSION_NUMBER = env.APP_VERSION_NUMBER || '2.1.7';
+const BUILD_NUMBER = parseInt(env.APP_BUILD_NUMBER) || 88;
+const VERSION_NUMBER = env.APP_VERSION_NUMBER || '2.1.8';
 
 // Used by the 'Create Public Link' functionality
 export const VERIFIER_PLUS_URL = env['VERIFIER_PLUS_URL'] || 'https://verifierplus.org';
