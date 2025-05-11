027524b229b14e360ad076888e6e4241d645a5c0
Alex H <ahiguera@mit.edu>
2025-05-11 01:16:38 -0400

    code cleanup
    Signed-off-by: Alex H <ahiguera@mit.edu>

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,12 @@
 import type { Config } from 'jest';
 
-// Add all ESM or untranspiled packages used in your app here
 const packagesToTransformWithBabel = [
   '@react-native',
   '@digitalcredentials/http-client',
   'expo-secure-store',
   'expo-modules-core', 
   'react-native-fs',
-  'react-native', // core RN package
+  'react-native',
 ];
 
 const transformIgnorePatterns = [
@@ -21,7 +20,6 @@ const config: Config = {
   },
   transformIgnorePatterns,
   moduleNameMapper: {
-    // Optional: Stub out images/assets in Jest
     '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   setupFiles: ['<rootDir>/jest.setup.js'], // needed for mocks
