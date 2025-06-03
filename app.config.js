2ba3015f5905e37ad1134464f941e06ad26e8d6e
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-06-03 18:51:11 -0400

    Patch globalThis.base64FromArrayBuffer if needed.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ const BUILD_NUMBER = parseInt(env.APP_BUILD_NUMBER) || 90;
 const VERSION_NUMBER = env.APP_VERSION_NUMBER || '2.2.0';
 
 // Used by the 'Create Public Link' functionality
-export const VERIFIER_PLUS_URL = env['VERIFIER_PLUS_URL'] || 'https://verifierplus.org';
+export const VERIFIER_PLUS_URL = env['VERIFIER_PLUS_URL'] || 'https://feditest.ngrok.dev';
 
 export const WAS_BASE_URL = 'https://storage.dcc.did.coop';
 
