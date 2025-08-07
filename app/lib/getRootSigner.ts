fff0c645ceefb7034c25142dc4f0770daa10f908
Omar <omar@devomar.info>
2025-08-07 18:38:43 +0300

    Update app/lib/getRootSigner.ts
    Co-authored-by: Dmitri Zagidulin <dzagidulin@gmail.com>
    Signed-off-by: Omar <omar@devomar.info>
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app/lib/getRootSigner.ts
+++ app/lib/getRootSigner.ts
@@ -4,7 +4,7 @@ import { WAS_KEYS } from '../../app.config';
 import { Ed25519VerificationKey2020 } from '@digitalcredentials/ed25519-verification-key-2020';
 
 export async function getRootSigner() {
-  const rootSignerStr = await AsyncStorage.getItem(WAS_KEYS.SIGNER_JSON);
+  const rootSignerSerializedKeypair = await AsyncStorage.getItem(WAS_KEYS.SIGNER_KEYPAIR);
   if (!rootSignerStr) {
     throw new Error('Root signer not found in wallet.');
   }
