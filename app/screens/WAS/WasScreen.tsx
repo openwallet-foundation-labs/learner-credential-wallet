86eef619eccbe52764ac2dce3d48626f067977b1
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-08-28 17:43:31 -0400

    Disable WAS Export Space button for now.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app/screens/WAS/WasScreen.tsx
+++ app/screens/WAS/WasScreen.tsx
@@ -5,7 +5,7 @@ import { navigationRef } from '../../navigation';
 import { Ed25519VerificationKey2020 } from '@digitalcredentials/ed25519-verification-key-2020';
 import { StorageClient } from '@wallet.storage/fetch-client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import FileReader from 'react-native-filereader';
+// import FileReader from 'react-native-filereader';
 import { v4 as uuidv4 } from 'uuid';
 import { WAS } from '../../../app.config';
 import { useThemeContext } from '../../hooks';
@@ -202,79 +202,79 @@ const WASScreen = () => {
     }
   };
 
-  const exportSpace = async () => {
-    try {
-      setStatus('loading');
-      setMessage('Exporting space...');
-
-      if (!connectionDetails) throw new Error('No connection details found');
-
-      const confirmed = await displayGlobalModal({
-        title: 'Export Space',
-        confirmText: 'Export',
-        cancelOnBackgroundPress: true,
-        body: (
-          <Text style={{ color: theme.color.textPrimary }}>
-            This will export your entire WAS space as a tarball file.
-          </Text>
-        )
-      });
-
-      if (!confirmed) {
-        setStatus('idle');
-        setMessage('');
-        return;
-      }
-
-
-      const signer = await getRootSigner();
-      const storage = getStorageClient();
-
-      const space = storage.space({
-        signer,
-        id: connectionDetails.spaceId as `urn:uuid:${string}`,
-      });
-
-      const response = await space.get({
-        headers: {
-          Accept: 'application/x-tar'
-        }
-      });
-
-      if (!response.ok) throw new Error(`Failed to export space. Status: ${response.status}`);
-
-      const blob = await response.blob?.();
-      if (!blob) throw new Error('Failed to get blob from response');
-
-      const fileName = `was-space-${connectionDetails.spaceId.split('urn:uuid:')[1]}.tar`;
-      console.log('fileName:', fileName);
-
-      const reader = new FileReader();
-
-      const base64 = await new Promise<string>((resolve, reject) => {
-        reader.onloadend = () => {
-          const dataUrl = reader.result as string;
-          const base64Data = dataUrl.split(',')[1]; // strip "data:*/*;base64,"
-          resolve(base64Data);
-        };
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
-      });
-
-      try {
-        await shareBinaryFile(fileName, base64, 'application/x-tar');
-      } catch (err) {
-        await shareBinaryFile(fileName, base64, 'application/x-tar');
-      }
-
-      setStatus('success');
-      setMessage('Space exported and ready to share!');
-    } catch (err) {
-      console.error('Error exporting space:', err);
-      setStatus('error');
-      setMessage(err instanceof Error ? `Error: ${err.message}` : 'Failed to export space');
-    }
-  };
+  // const exportSpace = async () => {
+  //   try {
+  //     setStatus('loading');
+  //     setMessage('Exporting space...');
+  //
+  //     if (!connectionDetails) throw new Error('No connection details found');
+  //
+  //     const confirmed = await displayGlobalModal({
+  //       title: 'Export Space',
+  //       confirmText: 'Export',
+  //       cancelOnBackgroundPress: true,
+  //       body: (
+  //         <Text style={{ color: theme.color.textPrimary }}>
+  //           This will export your entire WAS space as a tarball file.
+  //         </Text>
+  //       )
+  //     });
+  //
+  //     if (!confirmed) {
+  //       setStatus('idle');
+  //       setMessage('');
+  //       return;
+  //     }
+  //
+  //
+  //     const signer = await getRootSigner();
+  //     const storage = getStorageClient();
+  //
+  //     const space = storage.space({
+  //       signer,
+  //       id: connectionDetails.spaceId as `urn:uuid:${string}`,
+  //     });
+  //
+  //     const response = await space.get({
+  //       headers: {
+  //         Accept: 'application/x-tar'
+  //       }
+  //     });
+  //
+  //     if (!response.ok) throw new Error(`Failed to export space. Status: ${response.status}`);
+  //
+  //     const blob = await response.blob?.();
+  //     if (!blob) throw new Error('Failed to get blob from response');
+  //
+  //     const fileName = `was-space-${connectionDetails.spaceId.split('urn:uuid:')[1]}.tar`;
+  //     console.log('fileName:', fileName);
+  //
+  //     const reader = new FileReader();
+  //
+  //     const base64 = await new Promise<string>((resolve, reject) => {
+  //       reader.onloadend = () => {
+  //         const dataUrl = reader.result as string;
+  //         const base64Data = dataUrl.split(',')[1]; // strip "data:*/*;base64,"
+  //         resolve(base64Data);
+  //       };
+  //       reader.onerror = reject;
+  //       reader.readAsDataURL(blob);
+  //     });
+  //
+  //     try {
+  //       await shareBinaryFile(fileName, base64, 'application/x-tar');
+  //     } catch (err) {
+  //       await shareBinaryFile(fileName, base64, 'application/x-tar');
+  //     }
+  //
+  //     setStatus('success');
+  //     setMessage('Space exported and ready to share!');
+  //   } catch (err) {
+  //     console.error('Error exporting space:', err);
+  //     setStatus('error');
+  //     setMessage(err instanceof Error ? `Error: ${err.message}` : 'Failed to export space');
+  //   }
+  // };
 
   const renderConnectionDetails = () => {
     if (!connectionDetails) return null;
@@ -342,22 +342,22 @@ const WASScreen = () => {
               Delete/Unprovision Space
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              styles.connectButton,
-              { backgroundColor: theme.color.buttonPrimary },
-            ]}
-            onPress={exportSpace}
-          >
-            <Text
-              style={[
-                styles.connectButtonText,
-                { color: theme.color.textPrimary },
-              ]}
-            >
-              Export Space
-            </Text>
-          </TouchableOpacity>
+          {/*<TouchableOpacity*/}
+          {/*  style={[*/}
+          {/*    styles.connectButton,*/}
+          {/*    { backgroundColor: theme.color.buttonPrimary },*/}
+          {/*  ]}*/}
+          {/*  onPress={exportSpace}*/}
+          {/*>*/}
+          {/*  <Text*/}
+          {/*    style={[*/}
+          {/*      styles.connectButtonText,*/}
+          {/*      { color: theme.color.textPrimary },*/}
+          {/*    ]}*/}
+          {/*  >*/}
+          {/*    Export Space*/}
+          {/*  </Text>*/}
+          {/*</TouchableOpacity>*/}
         </View>
       </View>
     );
