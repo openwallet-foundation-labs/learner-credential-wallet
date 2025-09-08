c591ee623400597bede617f7e3463c59432f653c
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-09-07 22:06:32 -0400

    Update jest transform list to pass tests.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- test/verifier-core.test.ts
+++ test/verifier-core.test.ts
@@ -3,7 +3,7 @@ import { verifyPresentation } from '../app/lib/validate';
 import { mockCredential } from '../app/mock/credential';
 import { VerifiablePresentation } from '../app/types/presentation';
 
-describe.only('verifier-core usage', () => {
+describe('verifier-core usage', () => {
   it('should verify a VP', async () => {
     const mockVp: VerifiablePresentation = {
       '@context': ['https://www.w3.org/2018/credentials/v1'],
