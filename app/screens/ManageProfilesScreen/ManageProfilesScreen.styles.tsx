1e211caab6f0d5d6a70287eb81da905780d7db3b
Megha C <megha@oneorigin.us>
2025-09-15 12:47:27 +0530

    534: accessibility navigation issue fix for manage profile and choose profile
    Signed-off-by: Megha C <megha@oneorigin.us>

--- app/screens/ManageProfilesScreen/ManageProfilesScreen.styles.tsx
+++ app/screens/ManageProfilesScreen/ManageProfilesScreen.styles.tsx
@@ -3,9 +3,11 @@ import { createDynamicStyleSheet } from '../../lib/dynamicStyles';
 export default createDynamicStyleSheet(({ theme, mixins }) => ({
   container: {
     flexDirection: 'column',
-    justifyContent: 'flex-end',
     padding: 16,
     flex: 1,
+  },
+  listContainer: {
+    flexGrow: 1,
   },
   input: {
     ...mixins.input,
