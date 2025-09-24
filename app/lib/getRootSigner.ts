83e5ee1074eb19deb3de37987966ce00f1991689
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-09-23 20:41:50 -0400

    Do not throw error when root signer not found.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app/lib/getRootSigner.ts
+++ app/lib/getRootSigner.ts
@@ -4,13 +4,15 @@ import { WAS } from '../../app.config';
 import { Ed25519VerificationKey2020 } from '@digitalcredentials/ed25519-verification-key-2020';
 import { ISigner } from '@digitalcredentials/ssi';
 
-export async function getRootSigner(): Promise<ISigner> {
+export async function getRootSigner(): Promise<ISigner | undefined> {
   if (!WAS.enabled) {
-    throw new Error('WAS is not enabled.');
+    console.log('Cannot get root signer, WAS disabled in config.');
+    return;
   }
   const rootSignerSerializedKeypair = await AsyncStorage.getItem(WAS.KEYS.SIGNER_KEYPAIR);
   if (!rootSignerSerializedKeypair) {
-    throw new Error('Root signer not found in wallet.');
+    console.log('Cannot get root signer, key pair not in storage.');
+    return;
   }
 
   const key = await Ed25519VerificationKey2020.from(JSON.parse(rootSignerSerializedKeypair));
