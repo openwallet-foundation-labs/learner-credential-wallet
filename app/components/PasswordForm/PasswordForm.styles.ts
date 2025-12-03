2f475ee7e5939e15c0ff06772d6fe73f658888be
Megha C <megha@oneorigin.us>
2025-12-03 21:37:55 +0530

    Create link on onboarding screen during password creation
    Signed-off-by: Megha C <megha@oneorigin.us>

--- app/components/PasswordForm/PasswordForm.styles.ts
+++ app/components/PasswordForm/PasswordForm.styles.ts
@@ -1,7 +1,13 @@
 import { createDynamicStyleSheet } from '../../lib/dynamicStyles'
+import { Color } from '../../styles'
 
 export default createDynamicStyleSheet(() => ({
   inputSeparator: {
     height: 14
+  },
+  helperText: {
+    fontSize: 12,
+    color: Color.Gray500,
+    marginTop: 8
   }
 }))
