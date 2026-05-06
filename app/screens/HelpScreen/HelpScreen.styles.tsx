9cbcf324f03aa1d8e4ef4c57ccb8d78b01d44d89
Alex H <ahiguera@mit.edu>
2026-05-05 21:12:25 -0400

    Fix merge conflicts and lint issues
    Signed-off-by: Alex H <ahiguera@mit.edu>

--- app/screens/HelpScreen/HelpScreen.styles.tsx
+++ app/screens/HelpScreen/HelpScreen.styles.tsx
@@ -1,31 +1,31 @@
-import { createDynamicStyleSheet } from '../../lib/dynamicStyles';
+import { createDynamicStyleSheet } from '../../lib/dynamicStyles'
 
 export default createDynamicStyleSheet(({ theme, mixins }) => ({
   bodyContainerCenter: {
     flexGrow: 1,
     alignItems: 'center',
     padding: 16,
-    backgroundColor: theme.color.backgroundPrimary,
+    backgroundColor: theme.color.backgroundPrimary
   },
   image: {
     height: 72,
     resizeMode: 'contain',
-    marginTop: 30,
+    marginTop: 30
   },
   link: {
-    color: theme.color.linkColor,
+    color: theme.color.linkColor
   },
   paragraphCenter: {
     ...mixins.paragraphText,
     fontSize: theme.fontSize.medium,
     textAlign: 'center',
-    marginTop: 30,
+    marginTop: 30
   },
   aboutTitleBolded: {
     ...mixins.paragraphText,
     fontFamily: theme.fontFamily.bold,
     fontSize: theme.fontSize.medium,
     textAlign: 'center',
-    marginTop: 30,
-  },
-}));
+    marginTop: 30
+  }
+}))
